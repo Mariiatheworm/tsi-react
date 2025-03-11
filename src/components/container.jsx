8b14f3d6b9a7d07d8eefac9f33c3196a9d57f65b
mariiatheworm <map03032003@gmail.comm>
2025-03-11 22:27:34 +0900

    styled footer and header and added layout

---
+++ src/components/container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return <div className="p-4 bg-amber-300 px-[300px]">{children}</div>;
+};
+
+export default Container;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Container from "./container";
 
 const Header = () => {
   return (
-    <div>
-      Header
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contacts"}>Contacts</Link>
+    <header className="w-full flex flex-row justify-between gap-2 h-20 py-5 px-[300px]">
+      <div className="flex flex-row justify-left font-semibold font-inter gap-16">
+        <Link to={"/"} className="text-black">
+          <img src="/logo.png" alt="logo" className="w-[122px]" />
+        </Link>
+
+        <div className="flex justify-left gap-5">
+          <Link to={"/"} className="text-black">
+            Главная
+          </Link>
+          <Link to={"/about"} className="text-black">
+            О нас
+          </Link>
+          <Link to={"/contacts"} className="text-black">
+            Контакты
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="flex  gap-5">
+        <div className="flex flex-row gap-2">
+          <Link to={"/login"} className="text-black">
+            Вход
+          </Link>
+          <Link to={"/registration"} className="text-black">
+            Регистрация
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
